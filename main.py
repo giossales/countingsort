3a4009a6f071dbdb0927d1653f8c692e130adc83
Felipe Fraxe <felipe_fraxe@hotmail.com>
2023-02-14 09:44:25 -0400

    Tipos de dados explícitos, adição do segundo input do exemplo e função de verificação da ordenação

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-def maior(x):
-    m = x[0]
+def main() -> None:
+    input1: list = [5, 18, 11, 11, 15, 19, 12, 18]
+    input2: list = [12, 5, 1, 7, 11, 50, 9, 14, 15, 12]
+    print(f"{counting_sort(input1)} (Sorted: {is_sorted(input1)})")
+    print(f"{counting_sort(input2)} (Sorted: {is_sorted(input2)})")
+
+def maior(x: list) -> int:
+    m: int = x[0]
     for i in range(1, len(x)):
         if x[i] > m:
             m = x[i]
     return m
 
-
-def counting_sort(arr):
-    m = maior(arr)
-    aux = [0] * (m + 1)
+def counting_sort(arr: list) -> list:
+    m: int = maior(arr)
+    aux: list = [0] * (m + 1)
     for num in arr:
         aux[num] += 1
-    
-    j = 0
+    j: int = 0
     for i in range(len(aux)):
         while aux[i] > 0:
             arr[j] = i
@@ -20,9 +24,11 @@ def counting_sort(arr):
             j += 1
     return arr
 
-
-def main():
-    print(counting_sort([5, 18, 11, 11, 15, 19, 12, 18]))
+def is_sorted(arr: list) -> bool:
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
 
 
 if __name__ == "__main__":
